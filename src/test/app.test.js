f0b494ad8add1d9746a153499d30555bdee546f7
Aleksandar Diklic <rastasheep3@gmail.com>
2017-11-03 17:24:56 +0100

    Add testing library

---
+++ src/test/app.test.js
@@ -0,0 +1,9 @@
+const { expect } = require('chai');
+
+describe('Application tests', () => {
+  describe('initialization', () => {
+    it('should work', () => {
+      expect(true).to.be.true;
+    });
+  });
+});
